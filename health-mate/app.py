c61ea877a44725472a80d950ae671c09936a59a4
Ruby <ruby@YH-DKJ-PC-552.local>
2024-02-05 18:31:16 +0800

    change server name

--- health-mate/app.py
+++ health-mate/app.py
@@ -1,4 +1,4 @@
 import os
 dirname = os.path.dirname(__file__)
 st_app_name = os.path.join(dirname, 'st_app.py')
-os.system(f"streamlit run {st_app_name} --server.address 127.0.0.1 --server.port 7860")
+os.system(f"streamlit run {st_app_name} --server.address 0.0.0.0 --server.port 7860")
